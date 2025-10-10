aae02e3781313c9aab1a29c00b05645698776c83
Cl202182 <cl202182@g.unicamp.br>
2025-10-09 22:24:45 -0300

    modificação no CRUD Clientes

---
+++ src/Views/cliente/create.php
@@ -0,0 +1,5 @@
+<?php
+// Este arquivo simplesmente carrega o formulário.
+// A variável $cliente não existe aqui, o que faz o form.php
+// se comportar como um formulário de criação.
+require __DIR__ . '/form.php';

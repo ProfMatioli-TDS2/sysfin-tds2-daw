ff06cb4c9d45fda7af7c70c141c043f35c19df2e
RyanPilon09 <cl204249@g.unicamp.br>
2025-10-09 22:30:36 -0300

    tentando dnv

--- routes.php
+++ routes.php
@@ -1,5 +1,5 @@
 <?php
-// Retorna uma função que define todas as rotas para o dispatcher
+// Retorna uma função que define todas as rotas para o dispatcher.
 return function (FastRoute\RouteCollector $r) {
     //home;
     $r->addRoute('GET', '/', ['App\Controllers\HomeController', 'index']);

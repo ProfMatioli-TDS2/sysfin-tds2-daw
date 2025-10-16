c2a9762bd1810e5d0a353d03725dd7fbbe8dcd93
RyanPilon09 <cl204249@g.unicamp.br>
2025-10-15 22:36:28 -0300

    atualização final da tarefa 14

--- routes.php
+++ routes.php
@@ -14,6 +14,10 @@ return function (FastRoute\RouteCollector $r) {
     $r->addRoute(['GET', 'POST'], '/fornecedores/editar/{id:\d+}', ['App\Controllers\FornecedorController', 'edit']);
     $r->addRoute('POST', '/fornecedores/excluir/{id:\d+}', ['App\Controllers\FornecedorController', 'delete']);
     $r->addRoute('GET', '/fornecedores/relatorio', ['App\Controllers\FornecedorController', 'report']);
+
+    //compras
+    // Linha corrigida
+    $r->addRoute(['GET', 'POST'], '/compras', ['App\Controllers\CompraController', 'report']);
     $r->addRoute(['GET', 'POST'], '/compras/registrar', ['App\Controllers\CompraController', 'registrar']);
     $r->addRoute(['GET', 'POST'], '/compras/relatorio', ['App\Controllers\CompraController', 'report']);
 

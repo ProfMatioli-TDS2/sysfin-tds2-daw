70a5b7549b1cbe8437958dcf3f7a6e6f84a1d61f
Luan <luan>
2025-10-22 21:22:52 -0300

    interface de vendas

--- src/Views/layout/header.php
+++ src/Views/layout/header.php
@@ -36,9 +36,15 @@
                     <li class="nav-item">
                         <a class="nav-link" href="compras">Compras</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/sysfin-4dsn/">Vendas</a>
-                    </li>
+                    <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Vendas
+    </a>
+    <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="<?php echo BASE_URL; ?>index.php?url=/vendas">Listar Vendas</a></li>
+        <li><a class="dropdown-item"href="<?php echo BASE_URL; ?>index.php?url=/vendas/create">Registrar Nova Venda</a></li>
+    </ul>
+</li>
                 </ul>
 
                 <ul class="navbar-nav ms-auto">
